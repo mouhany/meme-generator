7648b3fa3b674238fcb49af9a44ad58d72a3d7bf
Mouhany <mouhany@gmail.com>
2024-09-12 20:48:17 +0700

    Edit state

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,18 +2,27 @@ import memesData from "../memesData";
 import { useState } from "react";
 
 function Meme() {
-  const [memeImage, setMemeImage] = useState("");
+  const [memeImage, setMemeImage] = useState(memesData);
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
 
   function getMemeImage() {
-    const memesAray = memesData.data.memes;
+    const memesAray = memeImage.data.memes;
     const randomNumber = Math.floor(Math.random() * memesAray.length);
     const url = memesAray[randomNumber].url;
-    console.log(url);
-    setMemeImage(url);
+    setMeme((prevState) => {
+      return {
+        ...prevState,
+        randomImage: url,
+      };
+    });
   }
 
   return (
-    <div className="my-12 p-4 sm:p-6 max-w-xl mx-auto border flex flex-col justify-between gap-9 sm:gap-6">
+    <div className="sm:my-12 lg:my-0 p-4 sm:p-6 max-w-xl mx-auto flex flex-col justify-between gap-9 sm:gap-6">
       <div className="flex flex-col sm:flex-row sm:justify-between gap-6">
         <div className="flex flex-col items-start gap-1">
           <label htmlFor="top-text" className="text-gray-600">
@@ -44,7 +53,7 @@ function Meme() {
       >
         Get a new meme image 🖼
       </button>
-      <img src={memeImage} />
+      <img src={meme.randomImage} />
     </div>
   );
 }
