e3d93f559c114c0a31e0c9ecdf37408d765da70c
Mouhany <mouhany@gmail.com>
2024-09-21 00:12:15 +0700

    Add top and bottom text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 function Header() {
   return (
-    <div className="flex flex-row justify-between items-center p-4 sm:p-6 bg-gradient-to-r from-seance-900 to-seance-700 text-white">
+    <header className="flex flex-row justify-between items-center p-4 sm:p-6 bg-gradient-to-r from-seance-900 to-seance-700 text-white">
       <div className="flex flex-row gap-2 items-center">
         <img src="./Troll Face.png" alt="Troll Face" className="size-6 sm:size-9"/>
         <div className="text-lg sm:text-2xl font-semibold">Meme Generator</div>
       </div>
       <div className="text-xs sm:text-base">React Course - Project 3</div>
-    </div>
+    </header>
   );
 }
 
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -9,6 +9,16 @@ function Meme() {
     randomImage: "http://i.imgflip.com/1bij.jpg",
   });
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevState) => {
+      return {
+        ...prevState,
+        [name]: value,
+      };
+    });
+  }
+
   function getMemeImage() {
     const memesAray = memeImage.data.memes;
     const randomNumber = Math.floor(Math.random() * memesAray.length);
@@ -22,25 +32,31 @@ function Meme() {
   }
 
   return (
-    <div className="sm:my-12 lg:my-0 p-4 sm:p-6 max-w-xl mx-auto flex flex-col justify-between gap-9 sm:gap-6">
+    <main className="sm:my-12 lg:my-0 p-4 sm:p-6 max-w-xl mx-auto flex flex-col justify-between gap-9 sm:gap-6">
       <div className="flex flex-col sm:flex-row sm:justify-between gap-6">
         <div className="flex flex-col items-start gap-1">
-          <label htmlFor="top-text" className="text-gray-600">
+          <label htmlFor="topText" className="text-gray-600">
             Top text
           </label>
           <input
-            id="top-text"
+            id="topText"
+            name="topText"
+            value={meme.topText}
+            onChange={handleChange}
             type="text"
             className="w-full sm:w-auto focus:outline-none focus:ring-2 focus:ring-seance-600 shadow-sm border rounded-md p-2"
             placeholder="Shut up"
           />
         </div>
         <div className="flex flex-col items-start gap-1">
-          <label htmlFor="bottom-text" className="text-gray-600">
+          <label htmlFor="bottomText" className="text-gray-600">
             Bottom text
           </label>
           <input
-            id="bottom-text"
+            id="bottomText"
+            name="bottomText"
+            value={meme.bottomText}
+            onChange={handleChange}
             type="text"
             className="w-full sm:w-auto focus:outline-none focus:ring-2 focus:ring-seance-600 shadow-sm border rounded-md p-2"
             placeholder="And take my money"
@@ -48,13 +64,17 @@ function Meme() {
         </div>
       </div>
       <button
-        className="p-2 rounded-md bg-gradient-to-r from-seance-900 to-seance-700 text-white"
+        className="getmemeimage"
         onClick={getMemeImage}
       >
         Get a new meme image 🖼
       </button>
-      <img src={meme.randomImage} />
-    </div>
+      <div className="relative">
+        <p className="impact top-1">{meme.topText}</p>
+        <p className="impact bottom-1">{meme.bottomText}</p>
+        <img className="w-full" src={meme.randomImage} />
+      </div>
+    </main>
   );
 }
 
